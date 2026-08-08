e19b73332a02fda8fc996257c6f4d2dc207b4737
Vaishnavi Agrawal <agrawalvaishnavi66@gmail.com>
2026-08-08 12:52:29 +0530

    backend url

--- frontend/app.py
+++ frontend/app.py
@@ -1,5 +1,11 @@
 import streamlit as st
 import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BACKEND_URL = os.getenv("BACKEND_URL")
 
 st.title("🤖 AI Chatbot")
 
@@ -8,7 +14,7 @@ user_input = st.text_input("Ask Anything")
 if st.button("Send"):
 
     response = requests.post(
-        "http://127.0.0.1:8000/chat",
+        f"{BACKEND_URL}",
         json={"message": user_input}
     )
 
